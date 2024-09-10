b01502b0d0c7828a9fec58dfba5086af3ae6e04e
Tommy's Macbook Pro(M3 Pro) <zwt03712@gmail.com>
2024-09-10 10:19:47 +1000

    update recommendation to p31

--- src/components/project-rankings.tsx
+++ src/components/project-rankings.tsx
@@ -29,23 +29,23 @@ const projects: Project[] = [
     title: "ICPC 组队平台",
     description:
       "一个中心化的系统，允许没有固定团队的学生注册并被匹配到具有类似经验的学生。",
-    tags: ["开发", "算法", "数据库", "Outlook接入"],
+    tags: ["开发", "全文搜索引擎", "数据库", "Outlook接入", "UI/UX"],
     tier: 0,
   },
   {
     id: "PRJ009",
     title: "UNSW Finance: 办公桌预约",
     description:
-      "一个办公桌预约系统，允许用户预约校园内的办公桌。(对前端要求会相对高点)",
-    tags: ["开发", "数据库", "实时通信", "Outlook接入"],
+      "一个办公桌预约系统，允许用户通过俯瞰图去预约校园内的办公桌。(对前端要求会相对高点)",
+    tags: ["开发", "数据库", "实时通信", "Outlook接入", "UI/UX"],
     tier: 1,
   },
   {
     id: "PRJ015",
     title: "自主身份证明 (Self-Sovereign Identity)",
     description:
-      "一个的自主身份证明系统，允许用户拥有自己的身份证明，而不是由政府或公司控制。",
-    tags: ["网络安全", "端到端", "开发", "移动端开发"],
+      "一个的自主身份证明系统，允许用户拥有自己的身份证明，而不是由政府或公司控制。（需要对安全技术有一定的了解，比如密码学）",
+    tags: ["网络安全", "端到端", "开发", "移动端开发", "密码学"],
     tier: 2,
   },
   {
@@ -53,7 +53,7 @@ const projects: Project[] = [
     title: "自适应在线化学教程",
     description:
       "一个自适应化学教学实验室在线教程的开发，允许学生以自己选择的节奏高效的学习。（网页分析的部分可能会有点难）",
-    tags: ["开发", "数据库", "数据可视化", "Moodle接入", "网页分析"],
+    tags: ["开发", "数据库", "数据可视化", "Moodle接入", "网页分析", "UI/UX"],
     tier: 2,
   },
   {
@@ -61,9 +61,17 @@ const projects: Project[] = [
     title: "HDR主题文件管理系统",
     // Produce a web application that maintains an overview and history of research interests andpublished documents for HDR students at CSE.
     description:
-      "制作一个网页应用，维护一个关于研究兴趣和已发表文档的概览和历史记录，供CSE的博士生使用。（虽然是管理系统，但是有踩雷的风险捏）",
+      "制作一个网页应用，维护一个关于研究兴趣和已发表文档的概览和历史记录，供CSE的博士生使用。（虽然是管理系统，但是有踩雷de）",
     tags: ["开发", "数据库"],
     tier: 1,
+  },
+  {
+    id: "PRJ031",
+    title: "自动化团队评估系统",
+    description:
+      "一个帮助Course Admin和 Lecturer 管理小组内成员冲突并公平地调整个人贡献的系统。",
+    tags: ["开发", "数据库", "Github接入", "Jira接入", "Outlook接入", "UI/UX"],
+    tier: 0,
   },
 ];
 
@@ -99,7 +107,7 @@ export function ProjectRankings() {
         </Button>
       </header>
       {/* <h1 className="text-3xl font-bold mb-6 text-center">Project Rankings</h1> */}
-      <ResponsiveMasonry columnsCountBreakPoints={{ 520: 1, 840: 2, 1024: 3 }}>
+      <ResponsiveMasonry columnsCountBreakPoints={{ 500: 1, 840: 2, 1024: 3 }}>
         <Masonry gutter="10px">
           {projects.map((project) => (
             <Card
